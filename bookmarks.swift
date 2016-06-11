c866bffce248adee2291ef227c5ebd893d7d9e3b
Alex Chan <alex@alexwlchan.net>
2016-06-11 17:15:25 +0100

    Add some simple bookmarks to the homepage

--- bookmarks.swift
+++ bookmarks.swift
@@ -1,10 +1,30 @@
 import Taylor
 
 
+// Set up a data structure for handling bookmarks.
+struct Bookmark {
+    let url: String
+    let title: String
+}
+
+let bookmarks = [
+    Bookmark(url: "https://github.com", title: "GitHub"),
+    Bookmark(url: "https://twitter.com", title: "Twitter"),
+]
+
+
 let server = Taylor.Server()
 
 server.get("/") { req, res in
-    res.bodyString = "Hello world!"
+
+    var bodyString = "<html><body><p>My bookmarks:</p><ul>"
+    for bookmark in bookmarks {
+        bodyString += "<li><a href=\"\(bookmark.url)\">\(bookmark.title)</li>"
+    }
+    bodyString += "</ul></html>"
+
+    res.bodyString = bodyString
+    res.headers["Content-Type"] = "text/html"
     return .Send
 }
 
