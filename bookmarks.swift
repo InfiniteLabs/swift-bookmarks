31b97799b4ab39f34316a206f1a15b78f6cd341b
Alex Chan <alex@alexwlchan.net>
2016-06-11 17:59:45 +0100

    Move the HTML for individual bookmarks into the template

--- bookmarks.swift
+++ bookmarks.swift
@@ -2,23 +2,22 @@ import Mustache
 import Taylor
 
 
-// Set up a data structure for handling bookmarks.
-struct Bookmark {
-    let url: String
-    let title: String
-}
-
 let bookmarks = [
-    Bookmark(url: "https://github.com", title: "GitHub"),
-    Bookmark(url: "https://twitter.com", title: "Twitter"),
+    ["url": "https://github.com", "title": "GitHub"],
+    ["url": "https://twitter.com", "title": "Twitter"],
 ]
 
 
-// Given the HTML body for a page, add the <html> and <head> content.
-func HTMLfromBody(bodyString: String) -> String {
+// Given a list of bookmarks, return the HTML required to render
+// them as a list.
+func HTMLforListOfBookmarks(bookmarks: [[String: String]]) -> String {
+    let data = [
+        "bookmarks": bookmarks,
+    ]
+
     do {
         let template = try Template(path: "./template.html")
-        return try template.render(Box(["bodyString": bodyString]))
+        return try template.render(Box(data))
     } catch {
         // This isn't great error handling -- we should really send
         // an error 500 status code.  Also, this error message is
@@ -29,22 +28,10 @@ func HTMLfromBody(bodyString: String) -> String {
 }
 
 
-// Given an array of Bookmarks, return the HTML required to list those
-// bookmarks on the page.
-func indexPage(bookmarks: [Bookmark]) -> String {
-    var bodyString = "<p>My bookmarks:</p><ul>"
-    for bookmark in bookmarks {
-        bodyString += "<li><a href=\"\(bookmark.url)\">\(bookmark.title)</li>"
-    }
-    bodyString += "</ul>"
-    return HTMLfromBody(bodyString)
-}
-
-
 let server = Taylor.Server()
 
 server.get("/") { req, res in
-    res.bodyString = indexPage(bookmarks)
+    res.bodyString = HTMLforListOfBookmarks(bookmarks)
     res.headers["Content-Type"] = "text/html"
     return .Send
 }
